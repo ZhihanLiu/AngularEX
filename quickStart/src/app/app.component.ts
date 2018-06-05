8a01218babb5530a1808be7f9baa72029eeaab33
zhihanliu <zhihanliu@hotmail.com>
2018-06-05 16:52:34 -0500

    2nd

--- quickStart/src/app/app.component.ts
+++ quickStart/src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'app';
+  flag = false;
+   call(msg:any){
+     alert(msg);
+    this. title = "asdasdasdasd";
+   }
+   change()
+{
+  this.flag = !this.flag;
+}
+
 }
